a2cb5ab7ba31f35a5d8c11d11c4f4b3249d397bb
Andrew Merrell <andrew.m.merrell@gmail.com>
2016-10-28 11:32:10 -0700

    adjust models to establish one to many relationship.

--- app/models/question.js
+++ app/models/question.js
@@ -3,5 +3,6 @@ import DS from 'ember-data';
 export default DS.Model.extend({
   title: DS.attr(),
   details: DS.attr(),
-  author: DS.attr()
+  author: DS.attr(),
+  answers: DS.hasMany('answer', {async: true}),
 });
