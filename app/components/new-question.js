84ee4aedf61405fbf7b9238ce74626126679a4e0
Andrew Merrell <andrew.m.merrell@gmail.com>
2016-10-28 10:26:41 -0700

    add field checking to saveQuestion method to ensure all fields are filled out.

--- app/components/new-question.js
+++ app/components/new-question.js
@@ -4,12 +4,20 @@ export default Ember.Component.extend({
 
   actions: {
     saveQuestion() {
+      debugger;
       var params = {
         title: this.get('title'),
         details: this.get('details'),
         author: this.get('author'),
       }
-      this.sendAction('saveQuestion', params);
+      if ((params.title === undefined) || (params.details === undefined) || (params.author === undefined)) {
+        alert("Please fill out all fields before submitting.");
+      } else {
+        this.set('title', "");
+        this.set('details', "");
+        this.set('author', "");
+        this.sendAction('saveQuestion', params);
+      }
     },
   }
 });
