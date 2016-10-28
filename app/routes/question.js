8655fefbe0dcb021d652c21dd6f3650015277396
Andrew Merrell <andrew.m.merrell@gmail.com>
2016-10-28 13:09:45 -0700

    prevent blank values from removing data in update form.

--- app/routes/question.js
+++ app/routes/question.js
@@ -12,7 +12,7 @@ export default Ember.Route.extend({
     },
     update(question, params) {
       Object.keys(params).forEach(function(key) {
-        if(params[key]!==undefined) {
+        if(params[key]!==undefined && params[key]!=="") {
           question.set(key, params[key]);
         }
       });
