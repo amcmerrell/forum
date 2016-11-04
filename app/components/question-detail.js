4bd55cb775af007e161792e2fd583e1a94464ed1
Andrew Merrell <andrew.m.merrell@gmail.com>
2016-11-04 09:55:51 -0700

    add service to pin questions to question board.

--- app/components/question-detail.js
+++ app/components/question-detail.js
@@ -1,11 +1,17 @@
 import Ember from 'ember';
 
 export default Ember.Component.extend({
+  questionBoard: Ember.inject.service(),
+
   actions: {
     delete(question) {
       if(confirm('Are you sure you want to delete this question?')) {
         this.sendAction('deleteQuestion', question);
       }
+    },
+    pinQuestion(question) {
+      console.log('got here');
+      this.get('questionBoard').add(question);
     },
   }
 });
